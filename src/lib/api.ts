b904ae4d94bc9a8403e7a45e74ed52b5aac49b81
277pawan <bpawan277@gmail.com>
2025-11-25 18:15:47 +0530

    chore: updated base url value

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,6 @@
 // API Client (function-based)
 
-const BASE_URL =
-  "https://habit-tracker-front-8rp6-r9kkvupr3-bpawan277gmailcoms-projects.vercel.app/api";
+const BASE_URL = "https://habit-tracker-front-8rp6.vercel.app/api";
 
 // token helpers
 const getToken = () =>
